492f3722d5a7e92a5e87ddb63c73fbf6170b3f96
UX DEMON OFFICIAL <aditya601642@gmail.com>
2026-08-15 19:51:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -536,7 +536,7 @@ KEYRING_FILE  = DIRS["data"] / "keyring.json"   # tiny local cache only
 # ┌──────────────────────────────────────────────────────────────┐
 # │  BOT TOKEN  add karo.   ││
 # └──────────────────────────────────────────────────────────────┘
-BOT_TOKEN_HARDCODED = "8800902962:AAHkIqQwrrzzI63DJBoPP9hipXnWqJMbPWA"   # ← ADD BOT TOKEN
+BOT_TOKEN_HARDCODED = "8800902962:AAH62sOXVNtRHaPf3KC6afbf_5QElP20dPs"   # ← ADD BOT TOKEN
 TOKEN = (
     os.environ.get("BOT_TOKEN")
     or os.environ.get("MAIN_BOT_TOKEN")
